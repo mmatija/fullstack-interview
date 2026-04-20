85192c4446baef76eb754b97e9e07346e5f26634
Matija Mocilac <mocilac.matija@gmail.com>
2026-04-20 17:41:26 +0200

    Encapsulate the concept of a server

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import http from 'http'
+import express from 'express'
+import membershipRoutes from './modern/routes/membership.routes'
+import { errorHandler } from './error-handler.middleware'
+// because of the javascript module, we need to use require to import the legacy routes
+const legacyMembershipRoutes = require('./legacy/routes/membership.routes')
+
+export class Server {
+
+    private app: express.Application;
+    private httpServer: http.Server | null = null;
+
+    constructor() {
+
+        this.app = express();
+
+        this.app.use(express.json())
+        this.app.use('/memberships', membershipRoutes);
+        this.app.use('/legacy/memberships', legacyMembershipRoutes);
+        this.app.use(errorHandler);
+    }
+
+    public start(port: number = 3099) {
+        this.httpServer = this.app.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}`)
+        })
+    }
+
+    public stop(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (!this.httpServer) {
+                return resolve()
+            }
+            this.httpServer.close((err) => {
+                this.httpServer = null
+                if (err) reject(err)
+                else resolve()
+            })
+        })
+    }
+
+}
