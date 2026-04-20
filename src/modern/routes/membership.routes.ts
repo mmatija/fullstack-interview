323e02aaf89b32896715c9935643f1add0462ce1
Matija Mocilac <mocilac.matija@gmail.com>
2026-04-21 00:42:17 +0200

    Implement an endpoint for creating membership

--- src/modern/routes/membership.routes.ts
+++ src/modern/routes/membership.routes.ts
@@ -1,19 +1,28 @@
 import express, { Request, Response } from "express"
 import memberships from "../../data/memberships.json"
 import membershipPeriods from "../../data/membership-periods.json"
+import { MembershipApplicationService } from "../services/membership-application-service"
+import moment from "moment"
 
-const router = express.Router();
+export function createMembershipRouter(membershipService: MembershipApplicationService) {
+    const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-    const response = memberships.map(membership => {
-        const periods = membershipPeriods.filter(period => period.membership === membership.id)
-        return { membership, periods }
+    router.get("/", (req: Request, res: Response) => {
+        const response = memberships.map(membership => {
+            const periods = membershipPeriods.filter(period => period.membership === membership.id)
+            return { membership, periods }
+        })
+        res.json(response)
     })
-    res.json(response)
-})
 
-router.post("/", (req: Request, res: Response) => {
-  throw new Error('not implemented')
-})
+    router.post("/", async (req: Request, res: Response) => {
+        const membership = await membershipService.createMembership(req.body)
+        res.json({
+            ...membership,
+            validFrom: moment(membership.validFrom).format("YYYY-MM-DD"),
+            validUntil: moment(membership.validUntil).format("YYYY-MM-DD"),
+        })
+    })
 
-export default router;
+    return router
+}
--- tests/membership/api/membership.routes.test.ts
+++ tests/membership/api/membership.routes.test.ts
@@ -4,10 +4,11 @@ import membershipStubs from "../../../src/data/memberships.json"
 import membershipPeriodsStubs from "../../../src/data/membership-periods.json"
 
 
-describe("Get Memberships", () => {
+describe("Membership API", () => {
 
     const server = new Server()
     const port = 8001
+    const userId = 2000
 
     beforeAll(async () => {
         await server.start(port)
@@ -17,14 +18,46 @@ describe("Get Memberships", () => {
         await server.stop()
     })
 
-    it("returns the list of memberships and their periods", async () => {
-        const response = await fetch(`http://localhost:${port}/memberships`);
-        const data = await response.json()
-        const expectedRespose = membershipStubs.map(membership => {
-            const periods = membershipPeriodsStubs.filter(period => period.membership === membership.id)
-            return { membership, periods }
+    describe("GET /memberships", () => {
+
+        it("returns the list of memberships and their periods", async () => {
+            const response = await fetch(`http://localhost:${port}/memberships`);
+            const data = await response.json()
+            const expectedRespose = membershipStubs.map(membership => {
+                const periods = membershipPeriodsStubs.filter(period => period.membership === membership.id)
+                return { membership, periods }
+            })
+            expect(data).toEqual(expectedRespose)
         })
-        expect(data).toEqual(expectedRespose)
     })
 
+    describe("POST /memberships", () => {
+        it("returns created membership", async () => {
+            const requestBody = {
+                name: "Test Membership",
+                userId: userId,
+                recurringPrice: 100,
+                validFrom: "2023-01-01",
+                paymentMethod: "credit card",
+                billingInterval: "monthly",
+                billingPeriods: 12
+            }
+            const response = await fetch(`http://localhost:${port}/memberships`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(requestBody)
+            })
+            const data = await response.json()
+            expect(data).toMatchObject({
+                id: expect.any(Number),
+                uuid: expect.any(String),
+                state: "expired",
+                assignedBy: "Admin",
+                validUntil: "2024-01-01",
+                ...requestBody
+            })
+        })
+    })
 })
