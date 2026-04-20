cff9f03b7dc206c138bb3732dd295a75b00083f0
Matija Mocilac <mocilac.matija@gmail.com>
2026-04-21 01:08:41 +0200

    Add missing file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import http from 'http'
 import express from 'express'
-import membershipRoutes from './modern/routes/membership.routes'
+import { createMembershipRouter } from './modern/routes/membership.routes'
 import { errorHandler } from './error-handler.middleware'
+import { InMemoryMembershipRepository } from './modern/repositories/in-memory-membership-repository'
+import { MembershipApplicationService } from './modern/services/membership-application-service'
 // because of the javascript module, we need to use require to import the legacy routes
 const legacyMembershipRoutes = require('./legacy/routes/membership.routes')
 
@@ -11,11 +13,13 @@ export class Server {
     private httpServer: http.Server | null = null;
 
     constructor() {
+        const membershipRepository = new InMemoryMembershipRepository()
+        const membershipService = new MembershipApplicationService(membershipRepository)
 
         this.app = express();
 
         this.app.use(express.json())
-        this.app.use('/memberships', membershipRoutes);
+        this.app.use('/memberships', createMembershipRouter(membershipService));
         this.app.use('/legacy/memberships', legacyMembershipRoutes);
         this.app.use(errorHandler);
     }
